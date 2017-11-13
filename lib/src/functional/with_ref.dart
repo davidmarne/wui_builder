ad2ed4c32ff4fd7bf23269dc1cb750982c21a4bf
David Marne <davemarne@gmail.com>
2017-11-12 23:08:49 -0700

    format

--- lib/src/functional/with_ref.dart
+++ lib/src/functional/with_ref.dart
@@ -2,21 +2,19 @@ import 'dart:html';
 import 'functional.dart';
 import '../../wui_builder.dart';
 
-typedef OutterP RefMapper<InnerP, E extends Element,
-    OutterP>(InnerP props, E ref);
+typedef OutterP RefMapper<InnerP, E extends Element, OutterP>(
+    InnerP props, E ref);
 
 /// [withRef] with pass a ref to the dom element to [baseComponent]
-ComponentEnhancer<InnerP, OutterP>
-    withRef<InnerP, E extends Element, OutterP>(
-            RefMapper<InnerP, E, OutterP> mapper) =>
-        (FunctionalComponent<OutterP> baseComponent) =>
-            (InnerP props) => new WithRef<InnerP, E, OutterP>(new WithRefProps()
-              ..mapper = mapper
-              ..baseProps = props
-              ..baseComponent = baseComponent);
+ComponentEnhancer<InnerP, OutterP> withRef<InnerP, E extends Element, OutterP>(
+        RefMapper<InnerP, E, OutterP> mapper) =>
+    (FunctionalComponent<OutterP> baseComponent) =>
+        (InnerP props) => new WithRef<InnerP, E, OutterP>(new WithRefProps()
+          ..mapper = mapper
+          ..baseProps = props
+          ..baseComponent = baseComponent);
 
-class WithRefProps<InnerP, E extends Element,
-    OutterP> {
+class WithRefProps<InnerP, E extends Element, OutterP> {
   RefMapper<InnerP, E, OutterP> mapper;
   InnerP baseProps;
   FunctionalComponent<OutterP> baseComponent;
