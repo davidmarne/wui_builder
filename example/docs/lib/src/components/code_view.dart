c7cd957d0c92c8c9d14675e3cd796fa17be5a7ae
David Marne <davemarne@gmail.com>
2017-12-16 21:44:04 -0700

    move docs

---
+++ example/docs/lib/src/components/code_view.dart
@@ -0,0 +1,110 @@
+import 'package:wui_builder/components.dart';
+import 'package:wui_builder/vhtml.dart';
+import 'package:wui_builder/wui_builder.dart';
+import 'package:prettify/prettify.dart';
+
+import '../demo_code_strings/demo_code_strings.dart';
+import '../demos/animation_frame.dart';
+import '../demos/context.dart';
+import '../demos/functional.dart';
+import '../demos/hello_world.dart';
+import '../demos/hocs.dart';
+import '../demos/idle_callback.dart';
+import '../demos/immutability.dart';
+import '../demos/props_example.dart';
+import '../demos/state_example.dart';
+import '../demos/triangle.dart';
+import '../demos/virtual_list.dart';
+import '../routes/routes.dart';
+
+class CodeViewProps {
+  Route route;
+}
+
+class CodeView extends PComponent<CodeViewProps> {
+  CodeView(CodeViewProps props) : super(props);
+
+  @override
+  void componentDidMount() {
+    prettyPrint();
+  }
+
+  @override
+  void componentDidUpdate(prevP, prevS) {
+    prettyPrint();
+  }
+
+  @override
+  VNode render() => new VDivElement()
+    ..className = 'columns'
+    ..children = [
+      new VDivElement()
+        ..className = 'column is-6 aside hero'
+        ..children = [
+          new VPreElement()
+            ..key = props.route
+            ..className = 'prettyprint lang-dart'
+            ..text = codeContent()
+        ],
+      new VDivElement()
+        ..className = 'column is-6 hero'
+        ..children = [currentExample()],
+    ];
+
+  VNode currentExample() {
+    switch (props.route) {
+      case Route.helloWorld:
+        return new HelloWorld(null);
+      case Route.props:
+        return new PropsExample(
+            new PropsExampleProps()..message = 'Hello World!');
+      case Route.state:
+        return new StateExample();
+      case Route.animationFrame:
+        return new AnimationFrame();
+      case Route.idleCallback:
+        return new IdleCallbackExample();
+      case Route.context:
+        return new ContextParent(null);
+      case Route.immutability:
+        return new ImmutabilityExample();
+      case Route.hocs:
+        return new HOCExample();
+      case Route.functional:
+        return tweet();
+      case Route.triangle:
+        return new TransformContainer();
+      case Route.virtualList:
+        return new VirtualScroll();
+    }
+    return new VDivElement()..text = 'throw';
+  }
+
+  String codeContent() {
+    switch (props.route) {
+      case Route.helloWorld:
+        return hello_world;
+      case Route.props:
+        return props_example;
+      case Route.state:
+        return state_example;
+      case Route.animationFrame:
+        return animation_frame;
+      case Route.idleCallback:
+        return idle_callback;
+      case Route.context:
+        return context;
+      case Route.immutability:
+        return immutability;
+      case Route.hocs:
+        return hocs;
+      case Route.functional:
+        return functional;
+      case Route.triangle:
+        return triangle;
+      case Route.virtualList:
+        return virtual_list;
+    }
+    return 'throw';
+  }
+}
---
+++ example/docs/lib/src/demos/triangle.dart
@@ -0,0 +1,177 @@
+import 'dart:async';
+import 'dart:html';
+
+import 'package:wui_builder/wui_builder.dart';
+import 'package:wui_builder/vhtml.dart';
+import 'package:wui_builder/components.dart';
+
+// This example demos the combination of setStateOnAnimationFrame
+// and setStateOnIdle together. The TransformContainer updates
+// the transform, which is high priority, every animation frame. While
+// the CounterStateHOC updates the numbers on each dot on idle callbacks.
+// This prevents the updating of the numbers from making the transform
+// animation chunky.
+
+// TransformContainer manages updating the css transform
+class TransformContainer extends SComponent<int> {
+  final int start;
+  TransformContainer() : start = new DateTime.now().millisecondsSinceEpoch;
+
+  @override
+  int getInitialState() => new DateTime.now().millisecondsSinceEpoch;
+
+  @override
+  BeforeAnimationFrame get beforeAnimationFrame => () {
+        setStateOnAnimationFrame(
+            (_, s) => new DateTime.now().millisecondsSinceEpoch - start);
+      };
+
+  void _styleBuilder(CssStyleDeclaration styleBuilder) {
+    final t = (state / 1000) % 10;
+    final scale = 1 + (t > 5 ? 10 - t : t) / 10;
+    final transform = 'scaleX(${scale / 2.1}) scaleY(0.7) translateZ(0.1px)';
+
+    styleBuilder
+      ..transform = transform
+      ..position = 'absolute'
+      ..transformOrigin = '0 0'
+      ..left = '50%'
+      ..top = '50%'
+      ..width = '10px'
+      ..height = '10px'
+      ..background = '#eee';
+  }
+
+  // UpdateBlocker prevents the whole component tree from rerendering
+  // every frame. We only want to update the style on the first
+  // VDivElement every frame
+  @override
+  VNode render() => new VDivElement()
+    ..styleBuilder = _styleBuilder
+    ..children = [
+      new UpdateBlocker(
+        new VDivElement()
+          ..children = [
+            new CounterStateHOC(),
+          ],
+      ),
+    ];
+}
+
+// CounterStateHOC is a high order component that mananges
+// increasing the number on each dot every second
+class CounterStateHOC extends SComponent<int> {
+  @override
+  int getInitialState() => 0;
+
+  @override
+  void componentDidMount() {
+    new Timer.periodic(const Duration(seconds: 1),
+        (_) => setStateOnIdle((_, prevState) => (prevState % 10) + 1));
+  }
+
+  @override
+  VNode render() => new SierpinskiTriangle(
+        new SierpinskiTriangleProps()
+          ..x = 0.0
+          ..y = 0.0
+          ..s = 1000.0
+          ..seconds = state,
+      );
+}
+
+class SierpinskiTriangleProps {
+  double x;
+  double y;
+  double s;
+  int seconds;
+}
+
+class SierpinskiTriangle extends PComponent<SierpinskiTriangleProps> {
+  final targetSize = 25.0;
+
+  SierpinskiTriangle(SierpinskiTriangleProps props) : super(props);
+
+  @override
+  bool shouldComponentUpdate(nextProps, _) => !(props.x == nextProps.x &&
+      props.y == nextProps.y &&
+      props.s == nextProps.s &&
+      props.seconds == nextProps.seconds);
+
+  @override
+  VNode render() {
+    if (props.s < targetSize)
+      return new Dot(
+        new DotProps()
+          ..x = props.x - (targetSize / 2.0)
+          ..y = props.y - (targetSize / 2.0)
+          ..size = targetSize
+          ..text = '${props.seconds}',
+      );
+
+    final e = window.performance.now() + 0.8;
+    while (window.performance.now() < e) {
+      // Artificially long execution time.
+    }
+
+    final s = props.s / 2;
+    return new VDivElement()
+      ..children = [
+        new SierpinskiTriangle(
+          new SierpinskiTriangleProps()
+            ..x = props.x
+            ..y = props.y - (s / 2.0)
+            ..s = s
+            ..seconds = props.seconds,
+        ),
+        new SierpinskiTriangle(
+          new SierpinskiTriangleProps()
+            ..x = props.x - s
+            ..y = props.y + (s / 2.0)
+            ..s = s
+            ..seconds = props.seconds,
+        ),
+        new SierpinskiTriangle(
+          new SierpinskiTriangleProps()
+            ..x = props.x + s
+            ..y = props.y + (s / 2.0)
+            ..s = s
+            ..seconds = props.seconds,
+        ),
+      ];
+  }
+}
+
+class DotProps {
+  double size;
+  double x;
+  double y;
+  String text;
+}
+
+class Dot extends Component<DotProps, Null> {
+  final center = 500;
+
+  Dot(DotProps props) : super(props);
+
+  void _styleBuilder(CssStyleDeclaration b) {
+    final s = props.size * 1.3;
+    b
+      ..position = 'absolute'
+      ..background = '#61dafb'
+      ..font = 'normal 15px sans-serif'
+      ..textAlign = 'center'
+      ..cursor = 'pointer'
+      ..width = '${s}px'
+      ..height = '${s}px'
+      ..left = '${props.x}px'
+      ..top = '${props.y}px'
+      ..borderRadius = '${s / 2}px'
+      ..lineHeight = '${s}px';
+  }
+
+  @override
+  VNode render() => new VDivElement()
+    ..styleBuilder = _styleBuilder
+    ..text = props.text;
+}
